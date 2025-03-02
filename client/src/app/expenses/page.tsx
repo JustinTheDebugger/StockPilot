591651aaf772313e933f9f3866f4c19d5abdd160
Justin <justin.tys@hotmail.com>
2025-03-02 13:58:54 +1300

    inital commit

---
+++ client/src/app/expenses/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useGetExpensesByCategoryQuery } from "@/state/api";
+import { useState } from "react";
+
+const Expenses = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const {
+    data: expensesData,
+    isLoading,
+    isError,
+  } = useGetExpensesByCategoryQuery();
+
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !expensesData) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    );
+  }
+
+  return <div>Expenses</div>;
+};
+
+export default Expenses;
---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
